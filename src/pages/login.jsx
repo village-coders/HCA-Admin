4d248720546702e68c992bdf03b3668b85120e01
Village-Coders <villagecoders7@gmail.com>
2026-03-22 00:33:29 +0100

    sdfdfdfdf

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -70,7 +70,7 @@ export default function Login() {
             <button
               type="button"
               onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-1/2 translate-y-1/2 text-gray-500 hover:text-gray-700"
+              className="absolute right-3 top-[33px] translate-y-1/2 text-gray-500 hover:text-gray-700"
             >
               {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
             </button>
